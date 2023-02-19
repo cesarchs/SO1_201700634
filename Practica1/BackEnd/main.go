02bbbef91036320fa61a5c72518c7b5098bcf573
cesarchs <cesarchamalesican@gmail.com>
2023-02-19 07:52:25 -0600

    Se agregaron peticiones

--- Practica1/BackEnd/main.go
+++ Practica1/BackEnd/main.go
@@ -30,8 +30,19 @@ func (c Calculadora) Resultado() float64 {
 		return 0
 	}
 }
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	//(*w).Header().Set("Access-Control-Allow-Origin", "POST")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
+}
 func CalculadoraHandler(w http.ResponseWriter, r *http.Request) {
+	header := w.Header()
+	header.Add("Access-Control-Allow-Origin", "*")
+	header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+	header.Add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+	setupResponse(&w, r)
 	switch r.Method {
 	case "GET":
 		queryValues := r.URL.Query()
@@ -42,7 +53,7 @@ func CalculadoraHandler(w http.ResponseWriter, r *http.Request) {
 		pi2, _ := strconv.ParseFloat(operando2, 64)
 		calculadora := Calculadora{Operando1: pi1, Operando2: pi2, Operacion: operacion}
 		resultado := calculadora.Resultado()
-		fmt.Fprintf(w, "El resultado es: %.3f", resultado)
+		fmt.Fprintf(w, "%.3f", resultado)
 	default:
 		fmt.Fprintf(w, "Metodo no permitido")
 	}
